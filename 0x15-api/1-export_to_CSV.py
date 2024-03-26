7cd1bfc2db9fe113fa58281d0082a25f64a49bc5
Mmeso1 <anisiubammesoma8@gmail.com>
2024-03-25 17:06:11 -0700

    task 2 finally overcome

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-""" Make an api request """
+""" Make a csv from the api request """
+import csv
 import requests
 from sys import argv
-import csv
 
 # The API endpoint
 base_url = "https://jsonplaceholder.typicode.com"
@@ -11,21 +11,12 @@ base_url = "https://jsonplaceholder.typicode.com"
 def fetch_employee_data(_id):
     user = requests.get(f"{base_url}/users/{_id}").json()
     todos = requests.get(f"{base_url}/todos?userId={_id}").json()
-    csv_filename = 'USER_ID.csv'
+    csv_filename = f"{_id}.csv"
 
-    done = []
-    total = 0
     with open(csv_filename, 'w', newline='') as file:
         writer = csv.writer(file, quoting=csv.QUOTE_ALL)
         for todo in todos:
-            writer.writerow([user['id'], user['name'], todo['completed'], todo['title']])
-            total += 1
-            if todo['completed']:
-                done.append(todo['title'])
-
-    print(f"Employee {user['name']} is done with tasks({len(done)}/{total}):")
-    for task in done:
-        print(f"\t {task}")
+            writer.writerow([user['id'], user['username'], todo['completed'], todo['title']])
 
 
 if __name__ == "__main__":
